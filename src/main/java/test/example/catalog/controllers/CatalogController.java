b5814ca56ea3d33618b81b6aca26741a67a82052
nanasinodonbei <yakke5305@gmail.com>
2024-10-02 17:12:00 +0900

    first commit

---
+++ src/main/java/test/example/catalog/controllers/CatalogController.java
@@ -0,0 +1,18 @@
+package test.example.catalog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class CatalogController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name, Model model){
+        model.addAttribute("name", name);
+        return"hello2";
+    }
+
+}
