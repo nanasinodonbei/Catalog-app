359fb81892a41e73c31a43129628c0b45f8588ee
nanasinodonbei <yakke5305@gmail.com>
2024-10-24 23:53:25 +0900

    キーワード検索実装

--- src/main/java/test/example/catalog/beans/SearchForm.java
+++ src/main/java/test/example/catalog/beans/SearchForm.java
@@ -10,6 +10,6 @@ import lombok.Data;
 public class SearchForm {
     
     private String brandId;
-    private String Keyword;
+    private String keyword;
 }
 
--- src/main/java/test/example/catalog/controllers/CatalogController.java
+++ src/main/java/test/example/catalog/controllers/CatalogController.java
@@ -1,18 +1,14 @@
 package test.example.catalog.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
-import test.example.catalog.beans.Brand;
-import test.example.catalog.beans.Instrument;
 import test.example.catalog.beans.SearchForm;
 import test.example.catalog.services.InstrumentService;
 
@@ -30,29 +26,19 @@ public class CatalogController {
     }
 
     @GetMapping("/inst")
-    public String inst(SearchForm serchFrom,Model model) {
-        log.info("検索条件:{}", serchFrom);
-        SearchForm condition = new SearchForm();
-        List<Brand> brands = new ArrayList<>();
-        // brands.add(new Brand("1", "ギブソン"));
-        // brands.add(new Brand("2", "SPR"));
-        // brands.add(new Brand("3", "グレッチ"));
-
-        List<Instrument> instruments = new ArrayList<>();
-
-        // instruments.add(new Instrument(1,"1", "ハミングバード","木", "?", 10000, "よい", null,
-        // null));
-        // instruments.add(new Instrument(2,"2", "ハミングバード2","木", "?", 103000, "めっちゃよい",
-        // null, null));
-        // instruments.add(new Instrument(3,"3", "Gretsch G5655TG Electromatic","木",
-        // "?", 1555000, "おそらくよい", null, null));
-
-
-        model.addAttribute("brands", instrumentService.getBrands());
-        model.addAttribute("instruments", instrumentService.getInst(condition));
-
-        log.debug("instruments: {}", instruments);
-        return "inst_catalog_list";
+public String inst(@ModelAttribute("searchForm") SearchForm searchForm, Model model) {
+    
+    if (searchForm == null) {
+        searchForm = new SearchForm();
     }
+
+    log.info("検索条件: {}", searchForm);
+
+    model.addAttribute("brands", instrumentService.getBrands());
+    model.addAttribute("instruments", instrumentService.getInst(searchForm));
+
+    log.debug("抽出結果: {}", model);
+    return "inst_catalog_list";
+}
 
 }
